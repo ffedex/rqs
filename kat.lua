191651e1384fa805854e316fc418d8d26fc22cc7
ffedex <bloxxdas@gmail.com>
2024-09-21 14:32:12 +0200

    Create kat.lua

---
+++ kat.lua
@@ -0,0 +1,6 @@
+--aimbot
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ttwizz/Open-Aimbot/master/source.lua", true))()
+-- IYR
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ffedex/0.1x/refs/heads/main/yessir.lua"))()
+-- esp
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ffedex/0.1x/refs/heads/main/esp.lua"))()
