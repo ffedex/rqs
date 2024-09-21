b09931bda3578f488b4abfad769bf6acc6cfdddf
ffedex <bloxxdas@gmail.com>
2024-09-21 14:48:15 +0200

    Update iykat.lua

--- iykat.lua
+++ iykat.lua
@@ -1,4 +1,4 @@
--- esp
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ffedex/0.1x/refs/heads/main/esp.lua"))()
 --aimbot
 loadstring(game:HttpGet("https://raw.githubusercontent.com/ttwizz/Open-Aimbot/master/source.lua", true))()
+-- esp
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ffedex/0.1x/refs/heads/main/esp.lua"))()
