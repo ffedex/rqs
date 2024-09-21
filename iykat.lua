7577487c2a18d6f6054147f15d2ae6bb4757bd6a
ffedex <bloxxdas@gmail.com>
2024-09-21 14:47:46 +0200

    Create iykat.lua

---
+++ iykat.lua
@@ -0,0 +1,4 @@
+-- esp
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ffedex/0.1x/refs/heads/main/esp.lua"))()
+--aimbot
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ttwizz/Open-Aimbot/master/source.lua", true))()
